5fa1cebe1622d6621b9a04ed9810cd49dcd6e8b9
Rameshkr <rameshkr423@gmail.com>
2025-03-29 08:05:45 +0530

    common root path

--- routes/front.js
+++ routes/front.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const router  = express.Router();
 const path = require('path');
+const rootdir = require('../utils/path');
 const frontController = require('../controllers/front');
     
 // Serve index.html for the root route
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../','views', 'index.html'));
+    res.sendFile(path.join(rootdir, '..','views', 'index.html'));
 });
 
 
 router.get('/service', (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'service-details.html'));
+    res.sendFile(path.join(rootdir, 'views', 'service-details.html'));
 });
 
 router.get('/starter', (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'starter-page.html'));
+    res.sendFile(path.join(__dirname, '..', 'views', 'starter-page.html'));
 });
 
 router.use((req, res, next) => {
-    res.status(404).send({ status: 404, message: 'Page URL not found' });
+    res.status(404).sendFile(path.join(__dirname, '..', 'views', '404.html'));
 });
 
 module.exports = router;
